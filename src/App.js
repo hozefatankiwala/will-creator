9c3006667e4c2f00cc877f3072a596aca315a6bc
Hozefa Tankiwala <hozefa.tankiwala@mavs.uta.edu>
2021-07-28 09:15:50 -0500

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import logo from './logo.svg';
+import Banner from './components/Banner/Banner';
+import MediaQuery from 'react-responsive'
 import './App.css';
+import Content from './components/Content/Content';
+import Fullscreen from './components/Fullscreen/Fullscreen'
+import Mobilescreen from './components/Mobilescreen/Mobilescreen'
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+
+          <Route path='/will-creator-tool'>
+            <header className="App-header">
+              <Banner />
+              <h1 id='will-generator'>Will Creator</h1>
+              <Content />
+            </header>
+          </Route>
+          <Route path={["/will-creator", "/"]}>
+            <MediaQuery minWidth={1224}>
+              <Fullscreen />
+            </MediaQuery>
+            <MediaQuery maxWidth={1000}>
+              <Mobilescreen />
+            </MediaQuery>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
